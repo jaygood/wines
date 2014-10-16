77d48d9b4e1fadc89814ea74f7f86f6ec644c25e
Jon <haygoodjon@gmail.com>
2014-10-16 13:11:53 -0700

    fix func.php to have corresponding function names

--- api/lib/func.php
+++ api/lib/func.php
@@ -1,102 +1,105 @@
 <?php
 
-function getUsers() {
-    $sql_query = "select `name`,`email`,`date`,`ip` FROM restAPI ORDER BY name";
-    try {
-        $dbCon = getConnection();
-        $stmt   = $dbCon->query($sql_query);
-        $users  = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $dbCon = null;
-        echo '{"users": ' . json_encode($users) . '}';
-    }
-    catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
+function getWines() {
+  $sql = "select * FROM wine ORDER BY name";
+  try {
+    $db = getConnection();
+    $stmt = $db->query($sql);
+    $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo '{"wine": ' . json_encode($wines) . '}';
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
 }
-function getUser($id) {
-    $sql = "SELECT `name`,`email`,`date`,`ip` FROM restAPI WHERE id=:id";
-    try {
-        $dbCon = getConnection();
-        $stmt = $dbCon->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $stmt->execute();
-        $user = $stmt->fetchObject();
-        $dbCon = null;
-        echo json_encode($user);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
+
+function getWine($id) {
+  $sql = "SELECT * FROM wine WHERE id=:id";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("id", $id);
+    $stmt->execute();
+    $wine = $stmt->fetchObject();
+    $db = null;
+    echo json_encode($wine);
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
 }
+
+function addWine() {
+  error_log('addWine\n', 3, '/var/tmp/php.log');
+  $request = Slim::getInstance()->request();
+  $wine = json_decode($request->getBody());
+  $sql = "INSERT INTO wine (name, grapes, country, region, year, description) VALUES (:name, :grapes, :country, :region, :year, :description)";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("name", $wine->name);
+    $stmt->bindParam("grapes", $wine->grapes);
+    $stmt->bindParam("country", $wine->country);
+    $stmt->bindParam("region", $wine->region);
+    $stmt->bindParam("year", $wine->year);
+    $stmt->bindParam("description", $wine->description);
+    $stmt->execute();
+    $wine->id = $db->lastInsertId();
+    $db = null;
+    echo json_encode($wine);
+  } catch(PDOException $e) {
+    error_log($e->getMessage(), 3, '/var/tmp/php.log');
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+function updateWine($id) {
+  $request = Slim::getInstance()->request();
+  $body = $request->getBody();
+  $wine = json_decode($body);
+  $sql = "UPDATE wine SET name=:name, grapes=:grapes, country=:country, region=:region, year=:year, description=:description WHERE id=:id";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("name", $wine->name);
+    $stmt->bindParam("grapes", $wine->grapes);
+    $stmt->bindParam("country", $wine->country);
+    $stmt->bindParam("region", $wine->region);
+    $stmt->bindParam("year", $wine->year);
+    $stmt->bindParam("description", $wine->description);
+    $stmt->bindParam("id", $id);
+    $stmt->execute();
+    $db = null;
+    echo json_encode($wine);
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
+function deleteWine($id) {
+  $sql = "DELETE FROM wine WHERE id=:id";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $stmt->bindParam("id", $id);
+    $stmt->execute();
+    $db = null;
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
+}
+
 function findByName($query) {
-    $sql = "SELECT * FROM restAPI WHERE name LIKE :query ORDER BY name";
-    try {
-        $dbCon = getConnection();
-        $stmt = $dbCon->prepare($sql);
-        $query = "%".$query."%";
-        $stmt->bindParam("query", $query);
-        $stmt->execute();
-        $users = $stmt->fetchAll(PDO::FETCH_OBJ);
-        $dbCon = null;
-        echo '{"user": ' . json_encode($users) . '}';
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
-function addUser(){
-    global $app;
-    $req = $app->request(); // Getting parameter with names
-    //? request()->post
-    $paramName = $req->params('name'); // Getting parameter with names
-    $paramEmail = $req->params('email'); // Getting parameter with names
-
-    $sql = "INSERT INTO restAPI (`name`,`email`,`ip`) VALUES (:name, :email, :ip)";
-    try {
-        $dbCon = getConnection();
-        $stmt = $dbCon->prepare($sql);
-        $stmt->bindParam("name", $paramName);
-        $stmt->bindParam("email", $paramEmail);
-        $stmt->bindParam("ip", $_SERVER['REMOTE_ADDR']);
-        $stmt->execute();
-        $user->id = $dbCon->lastInsertId();
-        $dbCon = null;
-        //or
-        //$app->redirect('/users')
-        echo json_encode($user);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
-function updateUser($id) {
-    global $app;
-    $req = $app->request();
-    $paramName = $req->params('name');
-    $paramEmail = $req->params('email');
-
-    $sql = "UPDATE restAPI SET name=:name, email=:email WHERE id=:id";
-    try {
-        $dbCon = getConnection();
-        $stmt = $dbCon->prepare($sql);
-        $stmt->bindParam("name", $paramName);
-        $stmt->bindParam("email", $paramEmail);
-        $stmt->bindParam("id", $id);
-        $status->status = $stmt->execute();
-
-        $dbCon = null;
-        echo json_encode($status);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
-}
-function deleteUser($id) {
-    $sql = "DELETE FROM restAPI WHERE id=:id";
-    try {
-        $dbCon = getConnection();
-        $stmt = $dbCon->prepare($sql);
-        $stmt->bindParam("id", $id);
-        $status->status = $stmt->execute();
-        $dbCon = null;
-        echo json_encode($status);
-    } catch(PDOException $e) {
-        echo '{"error":{"text":'. $e->getMessage() .'}}';
-    }
+  $sql = "SELECT * FROM wine WHERE UPPER(name) LIKE :query ORDER BY name";
+  try {
+    $db = getConnection();
+    $stmt = $db->prepare($sql);
+    $query = "%".$query."%";
+    $stmt->bindParam("query", $query);
+    $stmt->execute();
+    $wines = $stmt->fetchAll(PDO::FETCH_OBJ);
+    $db = null;
+    echo '{"wine": ' . json_encode($wines) . '}';
+  } catch(PDOException $e) {
+    echo '{"error":{"text":'. $e->getMessage() .'}}';
+  }
 }
